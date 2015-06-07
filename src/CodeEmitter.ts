2799af41d656931fb2e61277233c4c7a83e296e8
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-07 22:16:34 +0900

    Add CodeEmitter

---
+++ src/CodeEmitter.ts
@@ -0,0 +1,24 @@
+import {
+  Expression,
+  OperatorExpression,
+  NumberExpression
+} from "./Expression";
+
+export default
+class CodeEmitter {
+
+  emitCode(expr: Expression): string {
+    if (expr instanceof OperatorExpression) {
+      const left = this.emitCode(expr.left);
+      const right = this.emitCode(expr.right);
+
+      return `${left} ${expr.operator} ${right}`;
+    }
+    else if (expr instanceof NumberExpression) {
+      return expr.value.toString();
+    }
+    else {
+      throw new Error(`Not supported expression: ${expr.constructor.name}`);
+    }
+  }
+}
