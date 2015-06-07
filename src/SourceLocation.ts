a253e3631ef7363701d399f1e26d68401cf99fbd
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-07 20:53:37 +0900

    Use public

--- src/SourceLocation.ts
+++ src/SourceLocation.ts
@@ -1,13 +1,7 @@
 
 export default
 class SourceLocation {
-  line: Number;
-  column: Number;
-  offset: Number;
 
-  constructor(line: Number, column: Number, offset: Number) {
-    this.line = line;
-    this.column = column;
-    this.offset = offset;
+  constructor(public line: number, public column: number, public offset: number) {
   }
 }
