60bfc305e2cd654305bf7adb9ec1699e70b40834
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-09 14:18:22 +0900

    Test Compiler

--- test/test.ts
+++ test/test.ts
@@ -3,12 +3,13 @@
 "use strict";
 
 import assert from "power-assert";
+import Compiler from "../src/Compiler";
 
 describe("macaron", () => {
   it("should compile arithmetic expression", () => {
     const src = "1 + (2 * 3) / 2 - 6";
-    const result = "";
-    //const result = macaron.compile(src);
+    const result = new Compiler().compile(src);
+    const expected = "((1 + ((2 * 3) / 2)) - 6)";
     assert(src === result);
   });
 });
