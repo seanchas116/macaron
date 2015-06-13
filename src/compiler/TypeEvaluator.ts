239dd7b377c345ea4142faa75019f783dd811781
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-13 20:17:37 +0900

    Split TypeEvaluator methods

--- src/compiler/TypeEvaluator.ts
+++ src/compiler/TypeEvaluator.ts
@@ -45,91 +45,115 @@ class TypeEvaluator {
 
   evaluate(ast: ExpressionAST, env: Environment): Expression {
     if (ast instanceof AssignmentAST) {
-      const right = this.evaluate(ast.right, env);
-      const type = (() => {
-        switch (ast.declaration) {
-        case "let":
-          return DeclarationType.Constant;
-        case "var":
-          return DeclarationType.Variable;
-        default:
-          return DeclarationType.Assignment;
-        }
-      })();
-
-      return env.addVariableExpression(type, ast.left, right);
+      return this.evaluateAssignment(ast, env);
     }
     else if (ast instanceof BinaryAST) {
-      const left = this.evaluate(ast.left, env);
-      const right = this.evaluate(ast.right, env);
-      if (left.type !== right.type) {
-        throw new TypeCheckError(
-          `Cannot perform "${ast.operator.name}" operation between "${left.type}" and "right.type"`,
-          ast.operator.location
-        );
-      }
-      return new OperatorExpression(ast.operator.name, ast.operator.location, left, right);
+      return this.evaluateBinary(ast, env);
     }
     else if (ast instanceof IdentifierAST) {
-      return env.getVariableExpression(ast);
+      return this.evaluateIdentifier(ast, env);
     }
     else if (ast instanceof NumberAST) {
-      return new NumberExpression(ast.value, ast.location);
+      return this.evalauteNumber(ast, env);
     }
     else if (ast instanceof FunctionAST) {
-      const subEnv = new Environment(env);
-      const params: IdentifierExpression[] = [];
-      for (const {name, type} of ast.parameters) {
-        const metaType = this.evaluate(type, env).type;
-        if (metaType instanceof MetaType) {
-          subEnv.addVariable(DeclarationType.Constant, name, metaType.type);
-          params.push(new IdentifierExpression(name.name, name.location, metaType.type));
-        }
-        else {
-          throw new TypeCheckError(
-            `Provided expression is not a type`,
-            type.location
-          );
-        }
-      }
-      const expressions = this.evaluateExpressions(ast.expressions, subEnv);
-      const paramTypes = params.map(p => p.type);
-      const type = new FunctionType(paramTypes, [], returnType(expressions));
-      return new FunctionExpression(params, expressions, type);
+      return this.evaluateFunction(ast, env);
     }
     else if (ast instanceof FunctionCallAST) {
-      const func = this.evaluate(ast.function, env);
-      const funcType = func.type;
-
-      const args = this.evaluateExpressions(ast.arguments, env);
-
-      if (funcType instanceof FunctionType) {
-        if (args.length < funcType.minParamCount || funcType.maxParamCount < args.length) {
-          throw new TypeCheckError(
-            `Cannot pass ${args.length} arguments for ${funcType.minParamCount}...${funcType.maxParamCount} parameter function`,
-            ast.location
-          );
-        }
-        funcType.parameters.forEach((type, i) => {
-          if (!args[i].type.isCastableTo(type)) {
-            throw new TypeCheckError(
-              `Cannot pass '${args[i].type.name}' to '${type.name}'`,
-              ast.arguments[i].location
-            );
-          }
-        });
-
-        return new FunctionCallExpression(func, args);
-      }
-      else {
-        throw new TypeCheckError(
-          `${funcType.name} is not an function`,
-          ast.location
-        );
-      }
+      return this.evaluateFunctionCall(ast, env);
     }
     else {
       throw new Error(`Not supported AST: ${ast.constructor.name}`);
+    }
+  }
+
+  evaluateAssignment(ast: AssignmentAST, env: Environment) {
+    const right = this.evaluate(ast.right, env);
+    const type = (() => {
+      switch (ast.declaration) {
+      case "let":
+        return DeclarationType.Constant;
+      case "var":
+        return DeclarationType.Variable;
+      default:
+        return DeclarationType.Assignment;
+      }
+    })();
+
+    return env.addVariableExpression(type, ast.left, right);
+  }
+
+  evaluateBinary(ast: BinaryAST, env: Environment) {
+    const left = this.evaluate(ast.left, env);
+    const right = this.evaluate(ast.right, env);
+    if (left.type !== right.type) {
+      throw new TypeCheckError(
+        `Cannot perform "${ast.operator.name}" operation between "${left.type}" and "right.type"`,
+        ast.operator.location
+      );
+    }
+    return new OperatorExpression(ast.operator.name, ast.operator.location, left, right);
+  }
+
+  evaluateIdentifier(ast: IdentifierAST, env: Environment) {
+    return env.getVariableExpression(ast);
+  }
+
+  evalauteNumber(ast: NumberAST, env: Environment) {
+    return new NumberExpression(ast.value, ast.location);
+  }
+
+  evaluateFunction(ast: FunctionAST, env: Environment) {
+    const subEnv = new Environment(env);
+    const params: IdentifierExpression[] = [];
+    for (const {name, type} of ast.parameters) {
+      const metaType = this.evaluate(type, env).type;
+      if (metaType instanceof MetaType) {
+        subEnv.addVariable(DeclarationType.Constant, name, metaType.type);
+        params.push(new IdentifierExpression(name.name, name.location, metaType.type));
+      }
+      else {
+        throw new TypeCheckError(
+          `Provided expression is not a type`,
+          type.location
+        );
+      }
+    }
+    const expressions = this.evaluateExpressions(ast.expressions, subEnv);
+    const paramTypes = params.map(p => p.type);
+    const type = new FunctionType(paramTypes, [], returnType(expressions));
+    return new FunctionExpression(params, expressions, type);
+  }
+
+  evaluateFunctionCall(ast: FunctionCallAST, env: Environment) {
+    const func = this.evaluate(ast.function, env);
+    const funcType = func.type;
+
+    const args = this.evaluateExpressions(ast.arguments, env);
+
+    if (funcType instanceof FunctionType) {
+      if (args.length < funcType.minParamCount || funcType.maxParamCount < args.length) {
+        throw new TypeCheckError(
+          `Cannot pass ${args.length} arguments for ${funcType.minParamCount}...${funcType.maxParamCount} parameter function`,
+          ast.location
+        );
+      }
+      funcType.parameters.forEach((type, i) => {
+        if (!args[i].type.isCastableTo(type)) {
+          throw new TypeCheckError(
+            `Cannot pass '${args[i].type.name}' to '${type.name}'`,
+            ast.arguments[i].location
+          );
+        }
+      });
+
+      return new FunctionCallExpression(func, args);
+    }
+    else {
+      throw new TypeCheckError(
+        `${funcType.name} is not an function`,
+        ast.location
+      );
     }
   }
 }
