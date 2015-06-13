02d1f6b3f25bdb27de6ea426f015988798594936
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-13 18:30:44 +0900

    Add number as meta type in default env

--- src/defaultEnvironment.ts
+++ src/defaultEnvironment.ts
@@ -1,13 +1,13 @@
 import DeclarationType from "./DeclarationType";
 import Environment from "./Environment";
 import {numberType} from "./nativeTypes";
-import {Type} from "./Type";
+import {MetaType} from "./Type";
 import {IdentifierAST} from "./AST";
 
 export default
 function defaultEnvironment() {
   const env = new Environment();
-  env.addVariable(DeclarationType.Constant, new IdentifierAST("number"), numberType);
-  
+  env.addVariable(DeclarationType.Constant, new IdentifierAST("number"), new MetaType(numberType));
+
   return env;
 }
