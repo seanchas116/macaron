ee48a8ccbaef1f2228f87a11a1d37fef6280d262
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-30 19:47:25 +0900

    Add OperatorKind

---
+++ src/compiler/OperatorKind.ts
@@ -0,0 +1,25 @@
+enum OperatorKind {
+  Equal,
+  LessThan,
+  LessThanEqual,
+  GreaterThan,
+  GreaterThanEqual,
+
+  Add,
+  Subtract,
+  Multiply,
+  Divide,
+  Remainder,
+  Exponential,
+  Negate,
+
+  BitwiseNot,
+  BitwiseAnd,
+  BitwiseXor,
+  BitwiseOr,
+  LeftShift,
+  RightShift,
+  UnsignedRightShift,
+}
+
+export default OperatorKind;
