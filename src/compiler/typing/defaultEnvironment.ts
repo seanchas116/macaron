c59b0b95d5fea3b390db7fbad2218fc5fc5a478b
Ryohei Ikegami <iofg2100@gmail.com>
2015-08-10 15:40:33 +0900

    Fix default env

--- src/compiler/typing/defaultEnvironment.ts
+++ src/compiler/typing/defaultEnvironment.ts
@@ -1,28 +1,18 @@
-import DeclarationType from "./DeclarationType";
 import Environment from "./Environment";
 import {
   numberType,
   stringType,
   voidType
 } from "./nativeTypes";
-import {
-  Type,
-  MetaType
-} from "./Type";
-import Identifier from "./Identifier";
-import SourceLocation from "../common/SourceLocation";
 
 export default
 function defaultEnvironment() {
   const env = new Environment();
 
-  function addType(name: string, type: Type) {
-    env.addVariable(DeclarationType.Constant, new Identifier(new SourceLocation(1, 1, 0), name), new MetaType(type));
-  }
-
-  addType("number", numberType);
-  addType("string", stringType);
-  addType("void", voidType);
+  env.addType("number", numberType);
+  env.addType("string", stringType);
+  env.addType("void", voidType);
+  env.addType("any", voidType);
 
   return env;
 }
