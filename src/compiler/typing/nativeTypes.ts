d2fc958c5ef802a7a0a08e416a219e3839dfd946
Ryohei Ikegami <iofg2100@gmail.com>
2015-08-10 16:01:24 +0900

    WIP fix native type ops

--- src/compiler/typing/nativeTypes.ts
+++ src/compiler/typing/nativeTypes.ts
@@ -4,6 +4,8 @@ import {
   NativeOperator
 } from "./Operator";
 
+import CallSignature from "./CallSignature";
+
 import {
   UnaryOperatorKind,
   BinaryOperatorKind,
@@ -20,16 +22,45 @@ const voidType = anyType;
 export
 const numberType = new Type("number");
 
-// all operators supported
-for (const [name, kind] of unaryOperatorKinds) {
-  numberType.unaryOperators.set(kind, new NativeOperator(name));
-}
-for (const [name, kind] of binaryOperatorKinds) {
-  numberType.binaryOperators.set(kind, new NativeOperator(name));
-}
+export
+const booleanType = new Type("boolean");
 
 export
-const stringType = new PrimitiveType("string");
+const stringType = new Type("string");
 
-// supports +
-stringType.binaryOperators.set(BinaryOperatorKind.Add, new NativeOperator("+"));
+function addNativeBinaryOp(type: Type, name: string, ret: Type = type) {
+  const opType = new Type(`${type} operator ${name}`);
+  opType.callSignatures.push(new CallSignature(type, [type], ret));
+  type.binaryOperators.set(name, new NativeOperator(name, opType));
+}
+
+function addNativeUnaryOp(type: Type, name: string, ret: Type = type) {
+  const opType = new Type(`${type} operator ${name}`);
+  opType.callSignatures.push(new CallSignature(type, [], ret));
+  type.unaryOperators.set(name, new NativeOperator(name, opType));
+}
+
+addNativeBinaryOp(numberType, "==", booleanType);
+addNativeBinaryOp(numberType, "<", booleanType);
+addNativeBinaryOp(numberType, "<=", booleanType);
+addNativeBinaryOp(numberType, ">", booleanType);
+addNativeBinaryOp(numberType, ">=", booleanType);
+
+addNativeBinaryOp(numberType, "+");
+addNativeBinaryOp(numberType, "-");
+addNativeBinaryOp(numberType, "*");
+addNativeBinaryOp(numberType, "/");
+addNativeBinaryOp(numberType, "%");
+addNativeBinaryOp(numberType, "**");
+
+addNativeBinaryOp(numberType, "&");
+addNativeBinaryOp(numberType, "^");
+addNativeBinaryOp(numberType, "|");
+addNativeBinaryOp(numberType, "<<");
+addNativeBinaryOp(numberType, ">>");
+addNativeBinaryOp(numberType, ">>>");
+
+addNativeUnaryOp(numberType, "-");
+addNativeUnaryOp(numberType, "~");
+
+addNativeBinaryOp(stringType, "+");
