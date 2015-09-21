534b2aa9b2fec1729b9f243ee76facedf69b7e83
Ryohei Ikegami <iofg2100@gmail.com>
2015-09-21 17:51:11 +0900

    Implement GenericsParameterType#isAssignable

--- src/compiler/typing/type/GenericsParameterType.ts
+++ src/compiler/typing/type/GenericsParameterType.ts
@@ -5,4 +5,8 @@ class GenericsParameterType extends Type {
   constructor(name: string, public genericsType: Type, public genericsIndex: number, public restriction: Type) {
     super(name, [restriction]);
   }
+
+  isAssignable(other: Type): boolean {
+    return other === this;
+  }
 }
