95c93c13a7b1199edf6e5faa52d8b6d53ba453e3
Ryohei Ikegami <iofg2100@gmail.com>
2015-09-23 18:19:00 +0900

    Simplify UnionType

--- src/compiler/typing/type/UnionType.ts
+++ src/compiler/typing/type/UnionType.ts
@@ -5,45 +5,26 @@ import SourceLocation from "../../common/SourceLocation";
 import Member, {Constness} from "../Member";
 import TypeThunk from "../thunk/TypeThunk";
 
-function unionMembers(type: UnionType, members1: Map<string, Member>, members2: Map<string, Member>) {
+function unionMembers(location: SourceLocation, members1: Map<string, Member>, members2: Map<string, Member>) {
   const ret = new Map<string, Member>();
   for (const [name, member1] of members1) {
     if (!members2.has(name)) {
       continue;
     }
     const member2 = members2.get(name);
+
+    const member1Type = member1.type.get();
+    const member2Type = member2.type.get();
+
     if (member1.constness == Constness.Variable || member2.constness == Constness.Variable) {
       // nonvariant
-      if (member1.type.get().equals(member2.type.get())) {
-        ret.set(name, new Member(Constness.Variable, member1.type));
+      if (member1Type.equals(member2Type)) {
+        ret.set(name, new Member(Constness.Variable, member1Type));
       }
     }
 
-    const unionType = new TypeThunk(type.location, () => {
-      return new UnionType([member1.type.get(), member2.type.get()], type.location);
-    });
+    const unionType = new UnionType([member1Type, member2Type], location);
     ret.set(name, new Member(Constness.Constant, unionType));
-  }
-  return ret;
-}
-
-function unionOperators(type: UnionType, operators1: Map<string, Operator>, operators2: Map<string, Operator>) {
-  const ret = new Map<string, Operator>();
-  for (const [name, op1] of operators1) {
-    if (!operators2.has(name)) {
-      continue;
-    }
-    const op2 = operators2.get(name);
-    if (op1 instanceof MethodOperator && op2 instanceof MethodOperator) {
-      if (op1.methodName == op2.methodName) {
-        ret.set(name, new MethodOperator(type, op1.methodName));
-      }
-    }
-    else if (op1 instanceof NativeOperator && op2 instanceof NativeOperator) {
-      if (op1.nativeOperatorName === op2.nativeOperatorName) {
-        ret.set(name, new NativeOperator(op1.nativeOperatorName, type));
-      }
-    }
   }
   return ret;
 }
@@ -57,6 +38,7 @@ function unionCallSignatures(type: UnionType, signatures1: CallSignature[], sign
 
   for (const sig1 of signatures1) {
     for (const sig2 of signatures2) {
+      // TODO: intersection type
       if (deepEqual(sig1.params, sig2.params) && sig1.selfType === sig2.selfType) {
         const returnType = new UnionType([sig1.returnType, sig2.returnType], type.location);
         signatures.push(new CallSignature(sig1.selfType, sig1.params, returnType));
@@ -89,16 +71,8 @@ class UnionType extends Type {
     this.name = types.join("|");
 
     this.selfMembers = types.reduce((members, type) => {
-      return unionMembers(this, members, type.getMembers());
+      return unionMembers(loc, members, type.getMembers());
     }, new Map<string, Member>());
-
-    this.selfUnaryOperators = types.reduce((operators, type) => {
-      return unionOperators(this, operators, type.getUnaryOperators());
-    }, new Map<string, Operator>());
-
-    this.selfBinaryOperators = types.reduce((operators, type) => {
-      return unionOperators(this, operators, type.getBinaryOperators());
-    }, new Map<string, Operator>());
 
     this.selfCallSignatures = types.reduce((sigs, type) => {
       return unionCallSignatures(this, sigs, type.getCallSignatures());
