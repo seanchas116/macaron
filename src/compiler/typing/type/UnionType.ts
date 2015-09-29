d5b717b132fd3584b27faccab77832fb4a83e15b
Ryohei Ikegami <iofg2100@gmail.com>
2015-09-29 14:14:56 +0900

    Fix UnionType

--- src/compiler/typing/type/UnionType.ts
+++ src/compiler/typing/type/UnionType.ts
@@ -6,46 +6,6 @@ import SourceLocation from "../../common/SourceLocation";
 import Member, {Constness} from "../Member";
 import TypeThunk from "../thunk/TypeThunk";
 
-function unionMembers(location: SourceLocation, members1: Map<string, Member>, members2: Map<string, Member>) {
-  const ret = new Map<string, Member>();
-  for (const [name, member1] of members1) {
-    if (!members2.has(name)) {
-      continue;
-    }
-    const member2 = members2.get(name);
-
-    const member1Type = member1.type.get();
-    const member2Type = member2.type.get();
-
-    if (member1.constness == Constness.Variable || member2.constness == Constness.Variable) {
-      // nonvariant
-      if (member1Type.equals(member2Type, [])) {
-        ret.set(name, new Member(Constness.Variable, member1Type));
-      }
-    }
-
-    const unionType = new UnionType([member1Type, member2Type], location);
-    ret.set(name, new Member(Constness.Constant, unionType));
-  }
-  return ret;
-}
-
-function unionCallSignatures(location: SourceLocation, signatures1: CallSignature[], signatures2: CallSignature[]) {
-  const signatures: CallSignature[] = [];
-
-  for (const sig1 of signatures1) {
-    const sigs2 = signatures2.filter(s => s.params.length == sig1.params.length);
-    for (const sig2 of sigs2) {
-      signatures.push(new CallSignature(
-        new IntersectionType([sig1.selfType, sig2.selfType], location),
-        sig1.params.map((p, i) => new IntersectionType([p, sig2.params[i]], location)),
-        new UnionType([sig1.returnType, sig2.returnType], location)
-      ));
-    }
-  }
-  return signatures;
-}
-
 export default
 class UnionType extends Type {
   types: Type[];
@@ -53,6 +13,11 @@ class UnionType extends Type {
   // TODO: memoize
   constructor(types: Type[], loc: SourceLocation) {
     super("", [], loc);
+
+    if (types.length === 0) {
+      throw new Error("cannot create union with no type");
+    }
+
     const typeSet = new Set();
 
     for (const type of types) {
@@ -68,12 +33,64 @@ class UnionType extends Type {
     types = this.types = Array.from(typeSet);
     this.name = types.join(" | ");
 
-    this.selfMembers = types.reduce((members, type) => {
-      return unionMembers(loc, members, type.getMembers());
-    }, new Map<string, Member>());
-
-    this.selfCallSignatures = types.reduce((sigs, type) => {
-      return unionCallSignatures(loc, sigs, type.getCallSignatures());
-    }, []);
+    this.selfMembers = buildMembers(this.location, this.types);
+    this.selfCallSignatures = buildCallSignatures(this.location, this.types);
   }
+}
+
+function intersection<T>(xs: Set<T>, ys: Set<T>) {
+  const ret = new Set<T>();
+  for (const y of ys) {
+    if (xs.has(y)) {
+      ret.add(y);
+    }
+  }
+  return ret;
+}
+
+function buildMembers(location: SourceLocation, types: Type[]) {
+  const resultMembers = new Map<string, Member>();
+  const names = types
+    .map(t => new Set(t.getMembers().keys()))
+    .reduce(intersection);
+  for (const name of names) {
+    const members = types.map(t => t.getMember(name));
+    console.log(names);
+    console.log(members);
+    const memberTypes = members.map(m => m.type.get());
+    const type = new UnionType(memberTypes, location);
+    if (members.some(m => m.constness === Constness.Variable)) {
+      const settingType = new IntersectionType(memberTypes, location);
+      resultMembers.set(name, new Member(Constness.Variable, type, settingType));
+    } else {
+      resultMembers.set(name, new Member(Constness.Constant, type));
+    }
+  }
+  return resultMembers;
+}
+
+function buildCallSignatures(location: SourceLocation, types: Type[]) {
+  return types
+    .map(t => t.getCallSignatures())
+    .reduce((a, b) => unionCallSignatures(location, a, b));
+}
+
+function unionCallSignatures(location: SourceLocation, signatures1: CallSignature[], signatures2: CallSignature[]) {
+  // ((Foo, Bar), (Bar, Foo)) & ((Hoge, Piyo), (Piyo, Hoge))
+  // ->
+  // (Foo, Bar) & (Hoge, Piyo), (Foo, Bar) & (Piyo, Hoge), ...
+
+  const signatures: CallSignature[] = [];
+
+  for (const sig1 of signatures1) {
+    const sigs2 = signatures2.filter(s => s.params.length == sig1.params.length);
+    for (const sig2 of sigs2) {
+      signatures.push(new CallSignature(
+        new IntersectionType([sig1.selfType, sig2.selfType], location),
+        sig1.params.map((p, i) => new IntersectionType([p, sig2.params[i]], location)),
+        new UnionType([sig1.returnType, sig2.returnType], location)
+      ));
+    }
+  }
+  return signatures;
 }
