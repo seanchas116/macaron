c8569a6a14a0d4189b22ec15b1dd834413f05754
Ryohei Ikegami <iofg2100@gmail.com>
2015-10-24 21:52:06 +0900

    Remove typings

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="empower/empower.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="power-assert-formatter/power-assert-formatter.d.ts" />
-/// <reference path="power-assert/power-assert.d.ts" />
